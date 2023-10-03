a1a93ae6cc57902fae43b5373c8816b8ce312809
W0478181 <145702087+W0478181@users.noreply.github.com>
2023-10-03 10:49:58 -0300

    Update and rename W0478181_PROG1700_Simple_Algorith_V2_PY5.py to W0478181_PROG1700_Simple_Algorith_V3_PY5.py

--- W0478181_PROG1700_Simple_Algorith_V3_PY5.py
+++ W0478181_PROG1700_Simple_Algorith_V3_PY5.py
@@ -8,6 +8,7 @@
 #License: Creative Commons
 # #Repository: <https://github.com/W0478181/PROG1700Assignments>
 import datetime
+import sys
 current_year = datetime.datetime.now().year
 current_month = datetime.datetime.now().month
 age = None
@@ -15,25 +16,38 @@ month = None
 year_of_birth = None
 yes = "yes"
 no = "no"
+error_count = 0
 #This is a function to calculate someones birth year
 #Made some jokes to keep people entertained
+#Users have 3 attempts to enter proper values or else the algorih will quit
 def Birth_year_finder():
+    global error_count
     while True:
         try:
             age=int(input("Please enter age(numbers only),:")) 
             #Must be at least 1 to move forward
             if age <= 0:
                 print("Please enter a valid number.")
-            
+                error_count += 1
+                if error_count >= 3:
+                    print("3 strikes and your out,try again when your ready")
+                    sys.exit()
             elif age <= 4:
                 print("your not old enough yet bucko,Try agin in a few years.")
-                continue
-            
+                error_count += 1
+                if error_count >= 3:
+                    print("3 strikes and your out,try again when your ready")
+                    sys.exit()
+                
             else:
                 break
         except ValueError:
             print("Use a whole number c'mon")
-
+            error_count += 1
+            if error_count >= 3:
+                print("3 strikes and your out,try again when your ready")
+                sys.exit()
+            
 #Added this while statement to fix bug because if your birthday hasnt come yet you would get the wrong birth year        
     while True:
         try:
@@ -41,13 +55,21 @@ def Birth_year_finder():
            #This is to allows only the 12 months in a year and not break the code
             if month <= 0 or month > 12:
                 print("Please enter valid number.")
-                continue
+                error_count += 1
+                if error_count >= 3:
+                    print("3 strikes and your out,try again when your ready")
+                    sys.exit()
             
             else:
                 break
         
         except ValueError:
-            print("Use a whole number c'mon")    
+            print("Use a whole number c'mon")
+            error_count += 1
+            if error_count >= 3:
+                print("3 strikes and your out,try again when your ready")
+                sys.exit()
+                    
     #This line is for if the users birth month hasnt come yet it will change the answer to the proper birth year
     if month > current_month:
         year_of_birth = current_year - age - 1
@@ -66,6 +88,10 @@ def Birth_year_finder():
                 
                 else:
                     print("Please put yes or no")
+                    error_count += 1
+                    if error_count >= 3:
+                        print("3 strikes and your out,try again when your ready")
+                        sys.exit()
                     
     else:
         year_of_birth = current_year - age
@@ -78,14 +104,22 @@ def Birth_year_finder():
 
 Birth_year_finder()
 def Repeat_algorithm():
+    global error_count
     while True:
-        #conerts users response to lower case so function doesnt break
+        #coverts users response to lower case so function doesnt break
         repeat=(input("Would you like to try the algorithm again?(yes or no),:" ).lower())
         if repeat == yes:
             Birth_year_finder()
-
-        else:
+        
+        elif repeat == no:
             break
+        
+        else:
+            print("Please put yes or no")
+            error_count += 1
+            if error_count >= 3:
+                print("3 strikes and your out,try again when your ready")
+                sys.exit()
                
 #Option to Repeat function if user wants
 Repeat_algorithm()
