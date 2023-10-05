c76c0c80dc1b21813cbe0e6fe96138a513d10e0b
W0478181 <145702087+W0478181@users.noreply.github.com>
2023-10-05 15:16:16 -0300

    Simplified and shortened with some improvements

--- rps2.0.py
+++ rps2.0.py
@@ -1,32 +1,39 @@
 #W0478181
-# #Repository: <https://github.com/W0478181/PROG1700Assignments>
+#Repository: <https://github.com/W0478181/PROG1700Assignments>
 #Import Modules and variables
 import random
 import sys
-rock = 1
-paper = 2
-scissors = 3
+#List of options so users input can be displayed as what they played
+options = {1:"rock",2:"paper",3:"scissors"}
+#If error count gets to 3 its will give a message and run sys.exit()
 error_count = 0
+#Scoreboard for users to see 
+user_score = 0
+comp_score = 0
 #Program Control
 while True:
-    user_imput = input("Press 1 for rock, 2 for paper, 3 for scissors:")
-    #Validate the input to ensure the program has valid imput
+    user_input = input("Press 1 for rock, 2 for paper, 3 for scissors:")
+    #Validate the input to ensure the program has valid input
     #Return true is a digit is entered or else false
-    if user_imput.isdigit():
-        user_imput = int(user_imput)
-        if user_imput <=3 and user_imput >=1:
-            #Cast to an integer
-            # 1,2,3 numbers only
+    if user_input.isdigit():
+        user_input = int(user_input)
+        if user_input <=3 and user_input >=1:
+            #Cast to an integer 1,2,3 numbers only
             #Calculate computer's value
             computer_value = random.randrange(1,4,1)
-            # Determine the computer's choice
+            #Determine the computer's choice
             #Determine winner 
-            if user_imput == computer_value:
-                print("It's a tie!")
-            elif ((user_imput == 1 and computer_value == 3) or (user_imput == 2 and computer_value == 1) or (user_imput == 3 and computer_value == 2)):
-                print("You win!")
+            if user_input == computer_value:
+                print(f"It's a tie!You both played {options[user_input]}.")
+                print(f"User Score:{user_score},Al Gorithm Score:{comp_score}")
+            elif ((user_input == 1 and computer_value == 3) or (user_input == 2 and computer_value == 1) or (user_input == 3 and computer_value == 2)):
+                print(f"You win!You played {options[user_input]} and Al Gorithm played {options[computer_value]}.")
+                user_score += 1
+                print(f"User Score:{user_score},Al Gorithm Score:{comp_score}")
             else:
-                print("Computer wins!")
+                print(f"Computer wins!You played {options[user_input]} and Al Gorithm played {options[computer_value]}.")
+                comp_score += 1
+                print(f"User Score:{user_score},Al Gorithm Score:{comp_score}")
         else:
             print("Enter a valid number")
             error_count += 1
