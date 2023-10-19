a5e784d3a98589dc81319d6a028ba3514722f2e1
W0478181 <145702087+W0478181@users.noreply.github.com>
2023-10-19 14:12:58 -0300

    Short and simple

--- Guessing_gamev2.py
+++ Guessing_gamev2.py
@@ -1,15 +1,10 @@
 #W0478181
 #Repository: <https://github.com/W0478181/PROG1700Assignments>
 #Import Modules and variablesimport random
+import sys
 import random
 tries = 0
 #If users get 3 errors they will get message and program will close and User will only have 5 tries
-def error():
-    global error_count
-    error_count += 1
-    if error_count == 3:
-        print("3 strikes and you're out, try again when you're ready")
-        sys.exit()
 while True:
     computer_value = random.randrange(1, 10, 1)
     #Users must enter a name with no digits
@@ -19,18 +14,22 @@ while True:
     else:
         print("Please use letters only")
         continue
+    while tries < 5:
         #Users guess must be a number 1-10 only
-        user_guess = int(input("What is your guess?:"))
-        if 1 <= user_guess <= 10:
-            tries += 1
-            if user_guess == computer_value:
-                print(f"Congrats, you guessed my number on the {tries} try!")
-                break
-            elif user_guess < computer_value:
-                print("Your guess is too low.")
+        user_guess = (input("What is your guess?:"))
+        if user_guess.isdigit():
+            user_guess = int(user_guess)    
+            if 1 <= user_guess <= 10:
+                tries += 1
+                if user_guess == computer_value:
+                    print(f"Congrats, you guessed my number on the {tries} try!")
+                    break
+                elif user_guess < computer_value:
+                    print("Your guess is too low.")
+                else:
+                    print("Your guess is too high.")
             else:
-                print("Your guess is too high.")
-        else:
-            print("Enter a valid number")
-        if tries == 5:
-            print(f"That's 5 tries! The number was {computer_value}.")        sys.exit()
+                print("Enter a valid number")
+    if tries == 5:
+        print(f"That's 5 tries! The number was {computer_value}.")
+        sys.exit()
