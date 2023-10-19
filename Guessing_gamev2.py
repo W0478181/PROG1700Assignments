e323b92765d26888fd77676354653edc81fb9d2a
W0478181 <145702087+W0478181@users.noreply.github.com>
2023-10-19 14:09:50 -0300

    Short and simple

--- Guessing_gamev2.py
+++ Guessing_gamev2.py
@@ -1,9 +1,7 @@
 #W0478181
 #Repository: <https://github.com/W0478181/PROG1700Assignments>
 #Import Modules and variablesimport random
-import sys
 import random
-error_count = 0
 tries = 0
 #If users get 3 errors they will get message and program will close and User will only have 5 tries
 def error():
@@ -20,9 +18,7 @@ while True:
         print(f"Hi {user_name}, we are going to play a guessing game. I will pick a number from 1 to 10, and you will have 5 attempts to guess my number to win.")
     else:
         print("Please use letters only")
-        error()
         continue
-    while tries < 5:
         #Users guess must be a number 1-10 only
         user_guess = int(input("What is your guess?:"))
         if 1 <= user_guess <= 10:
@@ -36,8 +32,5 @@ while True:
                 print("Your guess is too high.")
         else:
             print("Enter a valid number")
-            error_count += 1
-            error()
-    if tries == 5:
-        print(f"That's 5 tries! The number was {computer_value}.")
-        sys.exit()
+        if tries == 5:
+            print(f"That's 5 tries! The number was {computer_value}.")        sys.exit()
